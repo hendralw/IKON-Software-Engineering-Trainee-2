c5d3593aa7dec0cd1ea1e9f7d119ce15887736ce
hendra lw <hndralw@gmail.com>
2023-01-14 02:48:32 +0700

    fixing redundant var

--- Day-9/demo.rest-api-jvs/src/main/java/com/example/demo/restapijvs/helloworld/service/HelloWorldServiceImpl.java
+++ Day-9/demo.rest-api-jvs/src/main/java/com/example/demo/restapijvs/helloworld/service/HelloWorldServiceImpl.java
@@ -4,20 +4,17 @@ import com.example.demo.restapijvs.helloworld.model.RequestModel;
 import com.example.demo.restapijvs.helloworld.model.ResponseModel;
 import com.example.demo.restapijvs.utils.ResponseApi;
 import com.example.demo.restapijvs.utils.ValidationApi;
-import jakarta.validation.Validator;
-import jakarta.validation.ValidatorFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
 @Service
 public class HelloWorldServiceImpl implements HelloWorldService {
-    ValidatorFactory validatorFactory;
-    Validator validator;
+    ResponseApi resp;
 
     @Override
     public ResponseApi getHelloWorld() {
         var data = new ResponseModel("Hello World");
-        var resp = new ResponseApi(
+        resp = new ResponseApi(
                 HttpStatus.OK.value(),
                 "Success get Data",
                 HttpStatus.OK.getReasonPhrase(),
@@ -29,8 +26,8 @@ public class HelloWorldServiceImpl implements HelloWorldService {
     @Override
     public ResponseApi createHelloWorld(RequestModel request) {
         var validation = new ValidationApi(request).Validate();
-        if (validation != "") {
-            var resp = new ResponseApi(
+        if (!validation.equals("")) {
+            resp = new ResponseApi(
                     HttpStatus.BAD_REQUEST.value(),
                     validation,
                     HttpStatus.BAD_REQUEST.getReasonPhrase(),
@@ -40,7 +37,7 @@ public class HelloWorldServiceImpl implements HelloWorldService {
         }
 
         var data = new ResponseModel(request.getMsg());
-        var resp = new ResponseApi(
+        resp = new ResponseApi(
                 HttpStatus.OK.value(),
                 "Success create Data",
                 HttpStatus.OK.getReasonPhrase(),
@@ -52,7 +49,7 @@ public class HelloWorldServiceImpl implements HelloWorldService {
     @Override
     public ResponseApi deleteHelloWorld() {
         var data = new ResponseModel("Hello World");
-        var resp = new ResponseApi(
+        resp = new ResponseApi(
                 HttpStatus.OK.value(),
                 "Success delete Data",
                 HttpStatus.OK.getReasonPhrase(),
